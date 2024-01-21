d18ebdad8a2e2988c0c36af1c4ed2c20062f2ef8
XyperCode <qboiwastaken@gmail.com>
2024-01-21 14:23:34 +0100

    Loading Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LogoPng from './logo.png'
+
+function Logo() {
+  console.log(LogoPng)
+  return (
+    <img src={LogoPng} alt="Logo" className='LoadingLogo'/>
+  );
+}
+
+function Loading() {
+  return (
+    <div className='Loading'>
+      <Logo/>
+      <span className="loader"/>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Loading/>
     </div>
   );
 }
